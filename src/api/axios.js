338b04f5293a3085ffb6539c0a1f2626614dacd1
hakim <hakimcheddani@gmail.com>
2026-04-16 14:48:35 +0100

    update api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://gestion-rh-backend-production.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
-  withCredentials: false, // ← changer ici
+  withCredentials: false,
 })
 
 api.interceptors.request.use((config) => {
